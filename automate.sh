93f52c586dac75c4a8e06cd197a7fb86b79d6be8
Ubuntu <ubuntu@ip-172-31-5-137.ap-southeast-2.compute.internal>
2023-12-29 10:01:03 +0000

    docker file added

--- automate.sh
+++ automate.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install python3-pip
-pip install django
-pip install openai
-pip install python-dotenv
-pip install typer
-pip install openai==0.28
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+sudo apt  install docker.io
+
+
+# Define the Dockerfile content
+DOCKERFILE_CONTENT=$(cat <<-EOF
+# Use an official Python runtime as a base image
+FROM python:3
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Update and install system packages
+RUN apt-get update && apt-get install -y \
+    # Add any additional system packages you may need here
+
+# Install Python dependencies
+RUN pip3 install --upgrade pip
+RUN pip3 install django openai python-dotenv typer==0.4.0 openai==0.28 click==7.1.2
+
+# Run Django migrations
+RUN python manage.py migrate
+
+# Start the Django development server
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8001"]
+EOF
+)
+
+# Save Dockerfile content to a file
+echo "$DOCKERFILE_CONTENT" > Dockerfile
+
+# Build Docker image
+sudo docker build -t my-django-app .
+
+# Run Docker container
+docker run -p 8001:8001 my-django-app
 
